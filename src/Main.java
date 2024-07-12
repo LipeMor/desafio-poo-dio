2f3e10ab07bdce921424c393500fc91a19d0707c
Filipe M. Santos <correio.filipe@gmail.com>
2024-07-12 11:36:42 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,11 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         Curso curso1 = new Curso();
-
         curso1.setTitulo("Curso Java");
         curso1.setDescricao("Descrição curso java");
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
-
         curso2.setTitulo("Curso Js");
         curso2.setDescricao("Descrição curso js");
         curso2.setCargaHoraria(4);
@@ -27,6 +25,9 @@ public class Main {
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria1);
+
+        double xp = curso2.calcularXP();
+        System.out.println(xp);
 
     }
 }
